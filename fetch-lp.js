125052b667656eaaf0e7ae7c3a42c1c96e4872d4
Christoshi <christoshi.2025@gmail.com>
2026-08-13 19:29:25 +0000

    Update fetch-lp.js

--- fetch-lp.js
+++ fetch-lp.js
@@ -1,12 +1,6 @@
 import fs from "fs/promises";
 
 const API_KEY = process.env.BITQUERY_API_KEY;
-const LAUNCHPADS = [
-  "0x23f8209572b4a1c2ad88a42749e830791fb027f1",
-  "0xad44d55e7f8337c3ce113fbb591486e85be104b2",
-  "0xce57498d3474dcc244dfb6710ffbe6d4441cd2b2",
-  "0x60d73b21cdf2ea846ab3d58699bbbb8f29d72491",
-];
 
 async function bitqueryFetch(query) {
   const res = await fetch("https://streaming.bitquery.io/graphql", {
@@ -22,22 +16,25 @@ async function bitqueryFetch(query) {
   return json;
 }
 
+// Testing one known poolId from the previous log
+const testPoolId = "8c4e99faf82381b202750311901f00b30f4e14296174556c5ca76a5e12f3396b";
+
 const query = `
 {
   EVM(network: robinhood) {
-    Events(
+    DEXPoolEvents(
       limit: {count: 5}
       orderBy: {descending: Block_Time}
       where: {
-        LogHeader: {Address: {in: ${JSON.stringify(LAUNCHPADS)}}}
-        Log: {Signature: {Name: {is: "TokenLaunched"}}}
+        PoolEvent: {Pool: {PoolId: {is: "${testPoolId}"}}}
       }
     ) {
-      Arguments {
-        Name
-        Value {
-          ... on EVM_ABI_Address_Value_Arg { address }
-          ... on EVM_ABI_Bytes_Value_Arg { hex }
+      Block { Time }
+      PoolEvent {
+        Pool { PoolId }
+        Liquidity {
+          AmountCurrencyAInUSD
+          AmountCurrencyBInUSD
         }
       }
     }
